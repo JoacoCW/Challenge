a29dfa325d2af315260d8008c50e8c03cc9d5445
JoaquinWeiss <joaco.w93@gmail.com>
2021-10-22 16:13:44 -0300

    completed JS Challenge

---
+++ JSChallenge/functions.js
@@ -0,0 +1,73 @@
+// Function that receives an array of positive integers and returns an array with only the input´s pair numbers
+
+let newarr = []
+
+const evens = (arr) => {
+    for ( let i = 0; i <= arr.length; i++){
+        if ( arr[i] % 2 == 0){
+            newarr.push(arr[i]);
+        }
+    } return newarr;
+}
+
+//Example
+
+let exampleArr = [1,2,3,4]
+
+console.log(evens(exampleArr));
+
+// Function that receives a string as parameter and returns a Boolean value indicating if the word is a palindromes or not
+
+const palindromeChecker = (str) => {
+    for ( let i = 0 ; i < str.length/2 ; i++){
+        if (str[i] !== str[str.length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+//Example
+
+console.log(palindromeChecker('ana'));  //True
+console.log(palindromeChecker('anythingElse')); // False
+
+// Create a Car object with tha mentioned attributes
+
+class Car {
+    constructor(make, model, turnedOn, engineDisplacement, year) {
+        this.make = make;
+        this.model = model;
+        this.turnedOn = turnedOn;
+        this.engineDisplacement = engineDisplacement;
+        this.year = year;
+    }
+    isOn() {
+        if (car.turnedOn) {
+            return true;
+        } else {
+            return false;
+        };
+    }
+    turnOn() {
+        if (car.turnedOn) {
+        } else {
+            turnedOn = true;
+        };
+    }
+    turnOff() {
+        if (car.turnedOn) {
+            turnedOn = false;
+        };
+    }
+    toString() {
+        return (this.year + ' ' + this.make + ' ' + this.model + ' ' + this.engineDisplacement);
+    }
+    getMarketingData() {
+        return (this.make + ' ' + this.model + ' ' + this.year);
+    }
+}
+
+const bicycle = new Car ( 'Vairo', 'Xr 3.8', 0, 0, 2019);
+
+console.log(bicycle.getMarketingData());
